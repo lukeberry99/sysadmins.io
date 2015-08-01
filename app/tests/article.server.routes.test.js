e93897840278f914b6c49011980fdf175661b4ce
Radoslav Ivanov <rivanov@gmail.com>
2015-08-01 20:26:33 +0300

    All but one of the tests for articles pass

--- app/tests/article.server.routes.test.js
+++ app/tests/article.server.routes.test.js
@@ -20,17 +20,12 @@ describe('Article CRUD tests', function() {
 	beforeEach(function(done) {
 		// Create user credentials
 		credentials = {
-			username: 'username',
-			password: 'password'
+			email: 'test@test.com',
+			password: 'testing'
 		};
-
 		// Create a new user
 		user = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-			email: 'test@test.com',
-			username: credentials.username,
+			email: credentials.email,
 			password: credentials.password,
 			provider: 'local'
 		});
